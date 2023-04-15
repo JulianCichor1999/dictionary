076c62dfe65d60171669188fd36b96625d6f4280
JulianCichorUP <julian.cichor@student.up.krakow.pl>
2023-04-15 23:17:37 +0200

    gotowy słownik

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
 function find_word(w) {
-  console.log(w);
   for (var i = 0; i < words.length; i++) {
     for (var j = 0; j < words[i].length; j++) {
       if(w === words[i][j])
       {
-        return true;
+        return j === 0 ? words[i][1] : words[i][0];
       }
     }
   }
   return false;
 }
 
-
 let words = [];
-let word = 'house';
+let word = '';
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'words.csv', true);
 xhr.onload = function () {
   if (this.status === 200) {
     const csv = this.responseText;
     const rows = csv.split('\n');
-    const data = rows.map(row => row.split(',').map(item => item.trim())); // usunięcie białych znaków
+    const data = rows.map(row => row.split(',').map(item => item.trim())); 
     Array.prototype.push.apply(words, data);
+    words.pop();
   }
-  words.pop();
-  console.log(find_word(word.trim())); // usunięcie białych znaków
 };
 xhr.send();
+
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  word = document.getElementById('pole').value;
+  if(!find_word(word))
+  {
+    document.getElementById("ann").innerHTML = "Nie znaleziono :(";
+  }
+  else
+  {
+    document.getElementById("ann").innerHTML = word + "-" +find_word(word);
+  }
+  
+});
+
+
+
+
+
